6b2f2d9d585bd5d7b24d8229a79e00c4c4c97f26
ChenXu <xuchen@thoughtworks.com>
2020-11-24 23:58:02 +0800

    feat: rename test case

--- MiniBlogTest/ControllerTest/ArticleControllerTest.cs
+++ MiniBlogTest/ControllerTest/ArticleControllerTest.cs
@@ -35,7 +35,7 @@ namespace MiniBlogTest.ControllerTest
         }
 
         [Fact]
-        public async void Should_create_post_fail_when_ArticleStore_unavailable()
+        public async void Should_create_article_fail_when_ArticleStore_unavailable()
         {
             var client = GetClient();
             string userNameWhoWillAdd = "Tom";
